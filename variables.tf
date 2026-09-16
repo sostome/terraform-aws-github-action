aaeb722f858e5f26a3dffc577e40e3205863eefe
sostome <baekjk@hanmail.net>
2026-09-16 14:21:07 +0900

    add env var

--- variables.tf
+++ variables.tf
@@ -38,6 +38,12 @@ variable "placeholder" {
 }
 
 variable "environment" {
+  type = string
   default = "dev"
   description = "Define infrastructure's environment"
+
+  validation {
+    condition = contains(["dev", "qa", "prod"], var.environment)
+    error_message = "The environment value must be dev, qa, or prod."
+  }
 }
